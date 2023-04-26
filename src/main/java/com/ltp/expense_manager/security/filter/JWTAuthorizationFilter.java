60989ffb00484147889a3c4b8e52349fb96be97b
Jatin Kansal <jatinkansal@Jatins-MacBook-Air.local>
2023-04-26 23:57:23 +0530

    Updated Spring Security

--- src/main/java/com/ltp/expense_manager/security/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/ltp/expense_manager/security/filter/JWTAuthorizationFilter.java
@@ -3,7 +3,6 @@ package com.ltp.expense_manager.security.filter;
 import java.io.IOException;
 import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,10 +17,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
-    @Autowired
     private Environment env;
 
     @Override
